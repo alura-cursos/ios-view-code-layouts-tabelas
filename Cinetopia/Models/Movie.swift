d8c31609df7a27cb1b0bb18ebfe8e05007845c0d
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-31 17:08:49 -0300

    aula 3.1 - implementando struct para representar os filmes

---
+++ Cinetopia/Models/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Cinetopia
+//
+//  Created by Giovanna Moeller on 31/10/23.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let image: String
+    let synopsis: String
+    let rate: Double
+    let releaseDate: String
+}
