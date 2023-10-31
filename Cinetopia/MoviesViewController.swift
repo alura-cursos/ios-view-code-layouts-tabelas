38a5449241ba3f77889e3a39d246abd8dd91fe64
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-31 16:58:09 -0300

    aula 1.2 - criando uma tabela

--- Cinetopia/MoviesViewController.swift
+++ Cinetopia/MoviesViewController.swift
@@ -9,10 +9,32 @@ import UIKit
 
 class MoviesViewController: UIViewController {
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
         setupNavigationBar()
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
     
     private func setupNavigationBar() {
