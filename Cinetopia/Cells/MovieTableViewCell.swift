b7334d189856b6ae1665c58ec617e8a41537240e
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-10-31 17:17:22 -0300

    aula 4.3 - construindo os outros elementos

--- Cinetopia/Cells/MovieTableViewCell.swift
+++ Cinetopia/Cells/MovieTableViewCell.swift
@@ -17,10 +17,29 @@ class MovieTableViewCell: UITableViewCell {
         return imageView
     }()
     
+    private lazy var movieTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Avatar"
+        label.font = .systemFont(ofSize: 20.0, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var movieReleaseDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Lançamento: 18/12/2009"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .white.withAlphaComponent(0.75)
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addSubviews()
         setupConstraints()
+        backgroundColor = .background // ou .clear
     }
     
     required init?(coder: NSCoder) {
@@ -29,13 +48,21 @@ class MovieTableViewCell: UITableViewCell {
     
     private func addSubviews() {
         addSubview(moviePosterImageView)
+        addSubview(movieTitleLabel)
+        addSubview(movieReleaseDateLabel)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             moviePosterImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-          moviePosterImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-          moviePosterImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+            moviePosterImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            moviePosterImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+            
+            movieTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            movieTitleLabel.leadingAnchor.constraint(equalTo: moviePosterImageView.trailingAnchor, constant: 16),
+            
+            movieReleaseDateLabel.topAnchor.constraint(equalTo: movieTitleLabel.bottomAnchor, constant: 8),
+            movieReleaseDateLabel.leadingAnchor.constraint(equalTo: moviePosterImageView.trailingAnchor, constant: 16)
         ])
     }
     
